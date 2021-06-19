521d065a5e35b6946a2fc4d5a75ac5096c5bfa80
AshwinSanthanam <ashwinsanthanam098@gmail.com>
2021-06-20 04:29:23 +0530

    spinner component

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ProductTileComponent } from './product-tile/product-tile.component';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 
 
 @NgModule({
   declarations: [
     NavBarComponent,
-    ProductTileComponent
+    ProductTileComponent,
+    SpinnerComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     NavBarComponent,
-    ProductTileComponent
+    ProductTileComponent,
+    SpinnerComponent
   ]
 })
 export class UiModule { }

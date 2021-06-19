95843a3c9ec0ad38db8975521f56f18713e2f9e2
AshwinSanthanam <ashwinsanthanam098@gmail.com>
2021-06-19 20:56:32 +0530

    home component setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
 import { UserLoginComponent } from './user-login/user-login.component';
 
 const routes: Routes = [
   { path: 'signup', component: SignupComponent },
-  { path: 'login', component: UserLoginComponent }
+  { path: 'login', component: UserLoginComponent },
+  { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({

036b8f912286392948e4af4fc0172b20d37a1d65
AshwinSanthanam <ashwinsanthanam098@gmail.com>
2021-06-20 05:39:42 +0530

    form group mapping

--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-login',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserLoginComponent implements OnInit {
 
+  public loginForm: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      'email': new FormControl('', [Validators.required, Validators.email]),
+      'password': new FormControl('', [Validators.required, Validators.minLength(8), Validators.maxLength(20)])
+    });
   }
 
+  public login(): void {
+    console.log(this.loginForm.value);
+  }
 }
